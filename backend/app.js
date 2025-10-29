e8603625064928df39f50a20ba23e181a55a6556
WillAxe <wilaxe1129@gmail.com>
2025-10-29 14:04:59 +0100

    installed bcrypt for password hashing

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,18 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require("cors")
+
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+path = require("path")
+
+//Routes for Users
+app.use("/jobmatch", require("./routes/userRoutes"))
+
+app.use(express.static(path.join(path.resolve(), "dist")))
+app.listen(console.log("Server is running on port", port))
