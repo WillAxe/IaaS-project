437176e2699e81a00e5a1efbeab8a2c0e28339bf
WillAxe <wilaxe1129@gmail.com>
2025-10-30 11:01:08 +0100

    Added two routes for users

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,20 @@
-require("dotenv").config()
+const dotenv = require("dotenv")
+dotenv.config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
 
 const port = process.env.PORT || 3000
 
+path = require("path")
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-path = require("path")
-
-app.get("/", (req, res) => {
-  res.send("Welcome to JobMatch API")
-})
-
-const planetRoutes = require("./routes/userRoutes")
-app.use("/jobmatch", userRoutes)
-
 //Routes for Users
-app.use("/jobmatch", require("./routes/userRoutes"))
+// const userRoute = require("./routes/userRoutes")
+// app.use("/jobmatch", userRoute)
 
 app.use(express.static(path.join(path.resolve(), "dist")))
 app.listen(console.log("Server is running on port", port))
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const userController = require("../controllers/userController")
+const userController = require("../controllers/userControllers")
 
 router.get("/users", userController.getAllUsers)
 router.post("/users", userController.createUser)
-router.get("/users:id", userController.getUserById)
