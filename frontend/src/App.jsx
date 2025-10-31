acb6c69cd54e3de5715b97e79383b5c6fbfe6efd
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-10-31 10:07:52 +0100

    Har skapat en inloggnings sida

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,14 @@
 // import { useState } from "react"
 import "./App.css"
-import Welcome from "./components/welcome"
+// import Welcome from "./components/welcome"
+import LoginPage from "./components/Welcomepage/welcome"  
 
-import {
-  createHashRouter,
-  Link,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom"
 function App() {
-  const router = createHashRouter([
-    {
-      children: [{ element: <Welcome />, path: "/" }],
-      element: (
-        <>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-            </ul>
-          </nav>
-          <main>
-            <Outlet></Outlet>
-          </main>
-        </>
-      ),
-    },
-  ])
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
 }
 
 export default App
