1a5c8bf3994188979c39627fea789a52f6962b90
Ibrahim Erfatpoor <ibrahim_feily@hotmail.com>
2025-11-03 14:18:41 +0100

    Nu kan man logga in och skickas till homepage vid inloggning

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import LoginPage from "./components/Welcomepage/welcome"
 import CreateAccount from "./components/Welcomepage/createAccount"
+import HomePage from "./components/Homepage/homePage"
 
 import {
   createHashRouter,
@@ -14,6 +15,7 @@ function App() {
       children: [
         { element: <LoginPage />, path: "/" },
         { element: <CreateAccount />, path: "/createAccount" },
+        { element: <HomePage />, path: "/homePage" },
       ],
       element: (
         <>
@@ -22,7 +24,9 @@ function App() {
               <li>
                 <Link to="/">Home</Link>
               </li>
+              <li>
               <Link to="/createAccount">createAccount</Link>
+              </li>
             </ul>
           </nav>
           <main>
