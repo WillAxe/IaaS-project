b5a200ac6c104aa894ae593c15035092b87b13d6
WillAxe <wilaxe1129@gmail.com>
2025-11-05 11:19:42 +0100

    added a sql for the job_posts table

---
+++ database/init.sql
@@ -0,0 +1,19 @@
+-- This is data for the database
+
+INSERT INTO Job_Posts(job_title, job_role, job_description, company_name) VALUES
+('Vi söker säljare', 'Säljare', 'Vi vill ha en erfaren säljare till vårt kontor som har tidigare erfarenhet inom försäljning Jobbet är ett kontorsjobb!', 'Will AB'),
+('HR Specialist', 'Human Resources', 'Ansvara för rekrytering, onboarding och personalfrågor.', 'PeopleFirst'),
+('Projekt Ledare', 'Management', 'Planera och leda mjukvaruprojekt enligt agila metoder.', 'Future Projects AB'), 
+('Frontend Developer', 'Software Development', 'Utveckla användargränssnitt i React', 'Techify AB'), 
+('System Administratör', 'IT Infrastructure', 'Underhåll och övervaka servrar och nätverk.', 'Nordic IT Solutions'), 
+('Data Analytiker', 'Data & Analytics','Analysera affärsdata och skapa rapporter i Power BI.', 'DataDriven AB'),
+('Lärare för en högstadieskola', 'Lärare inom Matematik', 'Arbete i team med andra lärare och lära ut matematik för en hel klass', 'Lamåsskolan'),
+('Senior spelutvecklare', 'spelutvecklare',  'Utveckla spel i team, med olika spelmotorer & verktyg','PixelForge Studios'),
+('Kommun Förvaltare inom Fristads Kommun', 'Kommun-Förvaltning', 'Uppdraget innefattar att bevaka personens rättigheter gentemot myndigheter och andra, och betala räkningar, söka bidrag eller sköta avtal.', 'Landets Domstolar'),
+('QA Testare', 'Quality Assurance', 'Testa webbapplikationer och skriva testfall.', 'BugHunter AB'), 
+('Cybersäkerhet Specialist', 'Säkerhet', 'Skydda system mot intrång och övervaka hot.',  'SecureTech'),
+('IT Support', 'Support', 'Hjälp användare med IT-problem och felsökning.','HelpDesk Sverige'), 
+('Digital Marknadsförare', 'Marketing', 'Planera och genomför digitala kampanjer.','Ibrahim AB'),
+('DevOps Engineer', 'Software Development', 'Automatisera CI/CD-pipelines och övervaka system.','CloudForge AB'), 
+('Content Writer', 'Marketing', 'Skriv artiklar och nyhetsbrev för företagets webbplats.','Wordify Media'), ('Fullstack-Developer', 'Software Development','Utveckla tjänster i ett större team med olika teknologier', 'Systemverket');
+
