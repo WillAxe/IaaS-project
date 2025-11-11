2eee8308ddca500378c39684ae85dcdae2e9dc07
WillAxe <wilaxe1129@gmail.com>
2025-11-11 15:41:27 +0100

    Added compose.yaml

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import { lazy, Suspense } from "react"
 import "./App.css"
 import LoginPage from "./components/Welcomepage/welcome"
 import CreateAccount from "./components/Welcomepage/createAccount"
 import HomePage from "./components/Homepage/homePage"
-import MyApplications from "./components/Homepage/myApplications"
+const MyApplications = lazy(() =>
+  import("./components/Homepage/myApplications")
+)
 import ProfilePage from "./components/Homepage/profilePage"
 import JobPost from "./components/Homepage/jobPost"
 import AccountSettings from "./components/Homepage/AccountSettings"
@@ -22,8 +25,19 @@ function App() {
       children: [
         { element: <LoginPage />, path: "/" },
         { element: <CreateAccount />, path: "/createAccount" },
-        { element: <HomePage />, path: "/homePage/:userid" },
-        { element: <MyApplications />, path: "/applications/user/:userid" },
+
+        {
+          element: <HomePage />,
+          path: "/homePage/:userid",
+        },
+        {
+          element: (
+            <Suspense fallback={<>Laddar...</>}>
+              <MyApplications />
+            </Suspense>
+          ),
+          path: "/applications/user/:userid",
+        },
         { element: <ProfilePage />, path: "/profilePage/:userid" },
         { element: <JobPost />, path: "/jobpost/:jobid" },
         { element: <AccountSettings />, path: "/account-settings/:userid" },
